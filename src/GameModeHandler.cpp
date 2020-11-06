cf5e3bebe34703138dea26f054815ada0d3a5feb
Andreas Rönnquist <andreas@ronnquist.net>
2020-11-06 23:51:34 +0100

    Add GameModeHandler namespace

---
+++ src/GameModeHandler.cpp
@@ -0,0 +1,90 @@
+/**
+ *
+ *	This file is part of PuzzeBlocks
+ *	Copyright (C) 2020 Andreas Rönnquist
+ *
+ *	PuzzeBlocks is free software: you can redistribute it and/or
+ *	modify it under the terms of the GNU General Public License as published
+ *	by the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	PuzzeBlocks is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with PuzzeBlocks.
+ *	If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+#include <memory>
+#include <list>
+#include <sstream>
+
+#include "GusGame/GusGame.h"
+
+
+using namespace Gus;
+
+using namespace LogLib;
+
+#include "GameMode.h"
+#include "GameModeMainMenu.h"
+#include "GameModeHandler.h"
+
+std::shared_ptr<GameMode> GameModeHandler::currentGameMode = nullptr;
+
+std::shared_ptr<GameModeMainMenu> GameModeHandler::gameModeMainMenu = nullptr;
+
+/**
+ *
+ */
+void GameModeHandler::initGameModes()
+{
+   LOG("Init GameModeHandler...");
+   
+   gameModeMainMenu = std::make_shared<GameModeMainMenu>();
+}
+
+
+/**
+ *
+ */
+void GameModeHandler::doneGameModes()
+{
+   LOG("Done GameModeHandler...");
+   
+   gameModeMainMenu.~shared_ptr();
+   
+   currentGameMode.~shared_ptr();
+}
+
+
+/**
+ *
+ */
+void GameModeHandler::switchGameMode(std::shared_ptr<GameMode> inGameMode)
+{
+   if (currentGameMode != nullptr) {
+      currentGameMode->leaveGameMode();
+   }
+
+   currentGameMode = inGameMode;
+   
+   if (currentGameMode != nullptr) {
+      currentGameMode->enterGameMode();
+   }
+}
+
+
+/**
+ *
+ */
+void GameModeHandler::draw()
+{
+   if (currentGameMode != nullptr) {
+      
+      currentGameMode->draw();
+   }
+}
---
+++ src/GameModeHandler.h
@@ -0,0 +1,40 @@
+/**
+ *
+ *	This file is part of PuzzeBlocks
+ *	Copyright (C) 2020 Andreas Rönnquist
+ *
+ *	PuzzeBlocks is free software: you can redistribute it and/or
+ *	modify it under the terms of the GNU General Public License as published
+ *	by the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	PuzzeBlocks is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with PuzzeBlocks.
+ *	If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef __HEADER_GAME_MODE_HANDLER_
+#define __HEADER_GAME_MODE_HANDLER_
+
+namespace GameModeHandler
+{
+   void initGameModes();
+   void doneGameModes();
+
+   void switchGameMode(std::shared_ptr<GameMode> gameMode);
+
+   void draw();
+
+   extern std::shared_ptr<GameMode> currentGameMode;
+   
+   extern std::shared_ptr<GameModeMainMenu> gameModeMainMenu;
+   
+};
+
+#endif /*__HEADER_GAME_MODE_HANDLER_*/
