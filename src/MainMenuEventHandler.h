a9e17450e1d9b83cd7504c3f947153d63bb6a4f4
Andreas Rönnquist <andreas@ronnquist.net>
2020-11-07 02:04:31 +0100

    Fix copyrights

--- src/MainMenuEventHandler.h
+++ src/MainMenuEventHandler.h
@@ -1,20 +1,20 @@
 /**
  *
- *	This file is part of PuzzeBlocks
+ *	This file is part of PuzzleBlocks
  *	Copyright (C) 2020 Andreas Rönnquist
  *
- *	PuzzeBlocks is free software: you can redistribute it and/or
+ *	PuzzleBlocks is free software: you can redistribute it and/or
  *	modify it under the terms of the GNU General Public License as published
  *	by the Free Software Foundation, either version 3 of the License, or
  *	(at your option) any later version.
  *
- *	PuzzeBlocks is distributed in the hope that it will be useful,
+ *	PuzzleBlocks is distributed in the hope that it will be useful,
  *	but WITHOUT ANY WARRANTY; without even the implied warranty of
  *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *	GNU General Public License for more details.
  *
  *	You should have received a copy of the GNU General Public License
- *	along with PuzzeBlocks.
+ *	along with PuzzleBlocks.
  *	If not, see <http://www.gnu.org/licenses/>.
  *
  */
