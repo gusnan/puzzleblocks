34384c224eda8f7243096d7a4a012567c06df3f0
Andreas Rönnquist <andreas@ronnquist.net>
2021-12-28 23:38:24 +0100

    Use a FindAllegro5.cmake that also works on Windows

--- cmake/FindAllegro5.cmake
+++ cmake/FindAllegro5.cmake
@@ -1,3 +1,4 @@
+
 # - Find allegro 5
 # Find the native ALLEGRO 5 includes and library
 #
@@ -6,60 +7,67 @@
 #  ALLEGRO_FOUND       - True if allegro found.
 
 
-IF (ALLEGRO_INCLUDE_DIR)
-  # Already in cache, be silent
-  SET(ALLEGRO_FIND_QUIETLY TRUE)
-ENDIF (ALLEGRO_INCLUDE_DIR)
+find_package(PkgConfig)
 
-FIND_PATH(ALLEGRO_INCLUDE_DIR allegro.h allegro5.h
-  /usr/local/include/allegro5
-  /usr/include/allegro5
-  $ENV{MINGDIR}/include/allegro5
-)
+# Find main allegro and assume the rest is there too
+pkg_check_modules(Allegro5_PKGCONF allegro-5)
 
-set(VERSION_NUMBER "5")
+# Perhaps use this later?
+# pkg_search_module(ALLEGRO5 REQUIRED allegro-5 allegro-5.0)
 
-if(UNIX AND NOT CYGWIN)
-	exec_program(pkg-config ARGS "allegro-${VERSION_NUMBER} allegro_ttf-${VERSION_NUMBER} allegro_memfile-${VERSION_NUMBER} allegro_image-${VERSION_NUMBER} allegro_primitives-${VERSION_NUMBER} allegro_audio-${VERSION_NUMBER} allegro_acodec-${VERSION_NUMBER} allegro_main-${VERSION_NUMBER} --libs" OUTPUT_VARIABLE ALLEGRO_LIBRARIES)
-	IF (NOT ALLEGRO_LIBRARIES)
-	    # FALL BACK TO 5.0
-	    set(VERSION_NUMBER "5.0")
-	    exec_program(pkg-config ARGS "allegro-${VERSION_NUMBER} allegro_ttf-${VERSION_NUMBER} allegro_memfile-${VERSION_NUMBER} allegro_image-${VERSION_NUMBER} allegro_primitives-${VERSION_NUMBER} allegro_audio-${VERSION_NUMBER} allegro_acodec-${VERSION_NUMBER} allegro_main-${VERSION_NUMBER} --libs" OUTPUT_VARIABLE ALLEGRO_LIBRARIES)
-	ENDIF (NOT ALLEGRO_LIBRARIES)
-	# Check if any of these checks have found if so then cause failure)
-	STRING(REGEX MATCH "not found" LIB_CHECK ALLEGRO_LIBRARIES)
-	IF (${LIB_CHECK})
-	    set(ALLEGRO_LIBRARIES "")
-	ENDIF (${LIB_CHECK})
-# FIXME - Figure out what to do for windows 
-#else(UNIX AND NOT CYGWIN)
-#	SET(ALLEGRO_NAMES allegro allegrolib allegrodll)
-#	FIND_LIBRARY(ALLEGRO_LIBRARY
-#	NAMES ${ALLEGRO_NAMES}
-#	PATHS /usr/lib /usr/local/lib $ENV{MINGDIR}/lib)
-endif(UNIX AND NOT CYGWIN)
+# MESSAGE("lib: ${Allegro5_PKGCONF_LIBRARY_DIRS}")
+# MESSAGE("include: ${Allegro5_PKGCONF_INCLUDE_DIRS}")
 
-IF (ALLEGRO_INCLUDE_DIR AND ALLEGRO_LIBRARIES)
-   SET(ALLEGRO_FOUND TRUE)
-   SET( ALLEGRO_LIBRARIES ${ALLEGRO_LIBRARIES} )
-   add_definitions(-DUSE_ALLEGRO5)
-ELSE (ALLEGRO_INCLUDE_DIR AND ALLEGRO_LIBRARIES)
-   SET(ALLEGRO_FOUND FALSE)
-   SET( ALLEGRO_LIBRARIES )
-ENDIF (ALLEGRO_INCLUDE_DIR AND ALLEGRO_LIBRARIES)
+# Include dir
+find_path(Allegro5_INCLUDE_DIR
+        NAMES allegro5/allegro5.h
+        PATHS ${Allegro5_PKGCONF_INCLUDE_DIRS}
+        )
 
-IF (ALLEGRO_FOUND)
-   IF (NOT ALLEGRO_FIND_QUIETLY)
-      MESSAGE(STATUS "Found Allegro 5: ${ALLEGRO_LIBRARIES}")
-   ENDIF (NOT ALLEGRO_FIND_QUIETLY)
-ELSE (ALLEGRO_FOUND)
-   IF (ALLEGRO_FIND_REQUIRED)
-      MESSAGE(STATUS "Looked for Allegro 5 libraries named ${ALLEGRO_NAMES}.")
-      MESSAGE(FATAL_ERROR "Could NOT find Allegro 5 library")
-   ENDIF (ALLEGRO_FIND_REQUIRED)
-ENDIF (ALLEGRO_FOUND)
+# message("include dir: ${Allegro5_INCLUDE_DIR}")
 
-MARK_AS_ADVANCED(
-  ALLEGRO_LIBRARIES
-  ALLEGRO_INCLUDE_DIR
-  )
+# Names of all libraries in Allegro, without versions
+set(ALLEGRO5_ALL_LIBRARIES
+        allegro allegro_image allegro_font
+        allegro_primitives allegro_ttf allegro_audio
+        allegro_dialog allegro_memfile allegro_acodec
+        allegro_color allegro_main allegro_physfs
+        )
+
+# set(ALLEGRO5_LIBRARIES "")
+
+# Find all libraries to link
+foreach(ALLEGRO5_ONE_LIBRARY ${ALLEGRO5_ALL_LIBRARIES})
+    # message("${ALLEGRO5_ONE_LIBRARY}")
+    find_library("${ALLEGRO5_ONE_LIBRARY}_AID" "${ALLEGRO5_ONE_LIBRARY}"
+            ${Allegro5_PKGCONF_LIBRARY_DIRS}
+            )
+    set(Allegro5_LIBRARIES
+            "${Allegro5_LIBRARIES}" "${${ALLEGRO5_ONE_LIBRARY}_AID}")
+    # MESSAGE("${Allegro5_LIBRARIES}")
+endforeach(ALLEGRO5_ONE_LIBRARY)
+# Remove first empty erroneous "library"
+list(REMOVE_AT Allegro5_LIBRARIES 0)
+# MESSAGE("${Allegro5_LIBRARIES}")
+# message("${ALLEGRO5_LIBRARIES}")
+# allegro_image allegro_font allegro_primitives
+#   allegro_ttf allegro_audio allegro_dialog allegro_acodec
+#   allegro_color allegro_main allegro_physfs
+
+
+
+# Set the include dir variables and the libraries and let libfind_process do the rest.
+set(ALLEGRO_INCLUDE_DIR "${Allegro5_INCLUDE_DIR}")
+set(ALLEGRO_LIBRARIES "${Allegro5_LIBRARIES}")
+
+if (ALLEGRO_INCLUDE_DIR AND ALLEGRO_LIBRARIES)
+	set(ALLEGRO_FOUND true)
+endif()
+# libfind_process(Allegro5)
+
+# MESSAGE("all libs: ${ALLEGRO_LIBRARIES}")
+# MESSAGE("all include: ${ALLEGRO_INCLUDE_DIR}")
+
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(Allegro5 DEFAULT_MSG ALLEGRO_LIBRARIES ALLEGRO_INCLUDE_DIR)
+
+MARK_AS_ADVANCED(ALLEGRO_INCLUDE_DIR ALLEGRO_LIBRARIES)
