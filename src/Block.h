cf4fa084be0b6f5509115e787254fe7cdd4bee22
Andreas Rönnquist <andreas@ronnquist.net>
2022-01-07 17:26:09 +0100

    Add Block class and Map class

---
+++ src/Block.h
@@ -0,0 +1,34 @@
+/**
+ *
+ *	This file is part of PuzzleBlocks
+ *	Copyright (C) 2022 Andreas Rönnquist
+ *
+ *	PuzzleBlocks is free software: you can redistribute it and/or
+ *	modify it under the terms of the GNU General Public License as published
+ *	by the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	PuzzleBlocks is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with PuzzleBlocks.
+ *	If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+#ifndef __HEADER_BLOCK_
+#define __HEADER_BLOCK_
+
+class Block
+{
+public:
+   Block();
+   virtual ~Block();
+   
+protected:
+   
+};
+
+#endif /*__HEADER_BLOCK_*/
---
+++ src/Map.h
@@ -0,0 +1,35 @@
+/**
+ *
+ *	This file is part of PuzzleBlocks
+ *	Copyright (C) 2022 Andreas Rönnquist
+ *
+ *	PuzzleBlocks is free software: you can redistribute it and/or
+ *	modify it under the terms of the GNU General Public License as published
+ *	by the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	PuzzleBlocks is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with PuzzleBlocks.
+ *	If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+#ifndef __HEADER_MAP_
+#define __HEADER_MAP_
+
+/**
+ *
+ */
+class Map
+{
+public:
+   Map();
+   virtual ~Map();
+
+};
+
+#endif
