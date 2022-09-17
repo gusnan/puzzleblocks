5b732bb684a90169ace02100ed15a62bb592c7be
Andreas Rönnquist <gusnan@librem.one>
2022-09-17 17:31:48 +0200

    Add a Main Menu Panel

---
+++ src/Gui/MainMenuPanel.h
@@ -0,0 +1,36 @@
+/**
+ *
+ *	This file is part of PuzzleBlocks
+ *	Copyright (C) 2022 Andreas Rönnquist
+ *
+ *	PuzzleBlocks is free software: you can redistribute it and/or
+ *	modify it under the terms of the GNU General Public License as published
+ *	by the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	PuzzleBlocks is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with PuzzleBlocks.
+ *	If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+#ifndef __HEADER_MAINMENUPANEL_
+#define __HEADER_MAINMENUPANEL_
+
+/**
+ *
+ */
+class MainMenuPanel : public Panel {
+public:
+   MainMenuPanel();
+   virtual ~MainMenuPanel();
+
+protected:
+
+};
+
+#endif /*__HEADER_MAINMENUPANEL_*/
