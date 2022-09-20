122fcd67a8ada7b585dc3b774442782934f2babe
Andreas Rönnquist <andreas@ronnquist.net>
2022-09-20 17:45:59 +0200

    Fix folders for finding GusGame

--- cmake/FindGusGame.cmake
+++ cmake/FindGusGame.cmake
@@ -7,61 +7,67 @@
 # GUSGAME_LIBRARY
 # 
 
+set(_pf_x86 "ProgramFiles\(x86\)")
+
+MESSAGE(STATUS "Found _pf_x86: $ENV{${_pf_x86}}")
+
 IF (WIN32)
-	FIND_PATH( GUSGAME_INCLUDE_DIR GusGame/GusGame.h
-		$ENV{PROGRAMFILES}/GusGame/include
-		$ENV{MINGDIR}/include/
-		$ENV{MINGDIR}/include/GusGame
-		$ENV{MINGDIR}
-		/usr/local
-		/usr/local/include
-		/usr/local/GusGame
-		/usr/local/include/GusGame
-		/local
-		/local/include
-		/local/GusGame
-		/local/include/GusGame
-		/GusGame
-		DOC "The directory where GusGame/GusGame.h resides")
-	FIND_LIBRARY( GUSGAME_LIBRARY
-		NAMES GUSGAME gusgame GusGame gusgame.a GusGame.a libgusgame.a libgusgame
-		PATHS
-		$ENV{PROGRAMFILES}/GusGame/lib
-		$ENV{MINGDIR}/GusGame/lib/
-		#$ENV{MINGDIR}/lib/GL
-		$ENV{MINGDIR}/
-		/local/lib
-		/lib/
-		/
-		DOC "The GusGame library")
+   FIND_PATH( GUSGAME_INCLUDE_DIR GusGame/GusGame.h
+      $ENV{PROGRAMFILES}/GusGame/include
+      $ENV{${_pf_x86}}/gusgame/include
+      $ENV{MINGDIR}/include/
+      $ENV{MINGDIR}/include/GusGame
+      $ENV{MINGDIR}
+      /usr/local
+      /usr/local/include
+      /usr/local/GusGame
+      /usr/local/include/GusGame
+      /local
+      /local/include
+      /local/GusGame
+      /local/include/GusGame
+      /GusGame
+      DOC "The directory where GusGame/GusGame.h resides")
+   FIND_LIBRARY( GUSGAME_LIBRARY
+      NAMES GUSGAME gusgame GusGame gusgame.a GusGame.a libgusgame.a libgusgame
+      PATHS
+      $ENV{PROGRAMFILES}/GusGame/lib
+      $ENV{${_pf_x86}}/gusgame/lib
+      $ENV{MINGDIR}/GusGame/lib/
+      #$ENV{MINGDIR}/lib/GL
+      $ENV{MINGDIR}/
+      /local/lib
+      /lib/
+      /
+      DOC "The GusGame library")
 ELSE (WIN32)
-	FIND_PATH( GUSGAME_INCLUDE_DIR GusGame/GusGame.h
-		#GusGame/GusGame.h
-		#$ENV{PROGRAMFILES}/GusGame/include
-		#$ENV{PROGRAMFILES}
-		/usr/include
-		/usr/local/include
-		/usr/local/include/gusgame
-		/usr/local/include
-		/sw/include
-		/opt/local/include
-		DOC "The directory where gusgame.h resides")
-	FIND_LIBRARY( GUSGAME_LIBRARY
-		NAMES GUSGAME gusgame GusGame libgusgame
-		PATHS
-		/usr/lib64
-		/usr/lib
-		/usr/local/lib64
-		/usr/local/lib
-		/sw/lib
-		/opt/local/lib
-		DOC "The GusGame library")
+   FIND_PATH( GUSGAME_INCLUDE_DIR GusGame/GusGame.h
+      #GusGame/GusGame.h
+      #$ENV{PROGRAMFILES}/GusGame/include
+      #$ENV{PROGRAMFILES}
+      /usr/include
+      /usr/local/include
+      /usr/local/include/gusgame
+      /usr/local/include
+      /sw/include
+      /opt/local/include
+      DOC "The directory where gusgame.h resides")
+   FIND_LIBRARY( GUSGAME_LIBRARY
+      NAMES GUSGAME gusgame GusGame libgusgame
+      PATHS
+      /usr/lib64
+      /usr/lib
+      /usr/local/lib64
+      /usr/local/lib
+      /sw/lib
+      /opt/local/lib
+      DOC "The GusGame library")
 ENDIF (WIN32)
 
 IF (GUSGAME_INCLUDE_DIR)
-	SET( GUSGAME_FOUND 1 CACHE STRING "Set to 1 if GusGame is found, 0 otherwise")
+   SET( GUSGAME_FOUND 1 CACHE STRING "Set to 1 if GusGame is found, 0 otherwise")
 ELSE (GUSGAME_INCLUDE_DIR)
-	SET( GUSGAME_FOUND 0 CACHE STRING "Set to 1 if GusGame is found, 0 otherwise")
+   SET( GUSGAME_FOUND 0 CACHE STRING "Set to 1 if GusGame is found, 0 otherwise")
 ENDIF (GUSGAME_INCLUDE_DIR)
 
 IF (GUSGAME_FOUND)
